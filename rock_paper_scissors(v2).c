85f5a02ff6158793518f35e7ce5cc2d50e99a7d7
Vikran Cute <47347693+VisalYun@users.noreply.github.com>
2019-02-27 15:22:36 +0700

    Rock Paper Scissor game with AI opponent in C

---
+++ rock_paper_scissors(v2).c
@@ -0,0 +1,37 @@
+#include<stdio.h>
+
+main(){
+	char choice, ans;
+	int play=0, r=0, p=0, s=0;
+	float guess_r, guess_s, guess_p;
+	do{
+	printf("Enter your choice(r/p/s): ");
+	scanf("%s", &choice);
+	while(choice!='r' && choice!='s' && choice!='p'){
+		printf("Please open your eyes!!!\n");
+		printf("________________________\n");
+		printf("Enter your choice(r/p/s): ");
+		scanf("%s", &choice);
+	}
+	play++;
+	if(choice=='r'){
+		printf("\nYou: Rock\n");
+		r++;
+		guess_r=(float)r/play;
+	}
+	else if(choice=='p'){
+		printf("\nYou: Paper\n");
+		p++;
+		guess_p=(float)p/play;
+	}
+	else{
+		printf("\nYou: Scissors\n");
+		s++;
+		guess_s=(float)s/play;
+	}
+	printf("Rock:%.2f\tPaper:%.2f\tScissors:%.2f\n",guess_r,guess_p,guess_s);
+	
+	printf("Play again(y/n): ");
+	scanf("%s", &ans);
+}while(ans='y');
+}
